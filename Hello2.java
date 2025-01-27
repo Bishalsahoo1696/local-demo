9c5cadf8fcd12ad4039bb70285bf14d42888874c
Bishalsahoo1696 <2021ABIT084@abit.edu.in>
2025-01-27 12:34:10 +0530

    Add feature2.java

--- Hello2.java
+++ Hello2.java
@@ -2,5 +2,6 @@ public class Hello2 {
 
     public static void main(String[] args) {
         System.out.println("Hello everyone");
+        System.out.println(" new line by feture1 branch");
     }
 }
